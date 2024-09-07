fd004fd37c085ba67627df2d7d02f5680ac8def1
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-09-07 15:50:26 +0200

    even simpler

--- src/main/java/org/example/MathStuff.java
+++ src/main/java/org/example/MathStuff.java
@@ -1,37 +1,45 @@
 package org.example;
 
 public class MathStuff {
-    private boolean sign;
-    private double mantissa;
-    private int exponent;
-    /**
-     * Prints the length of the vector (x, y, z)
-     * @param x the x component of the vector
-     * @param y the y component of the vector
-     * @param z the z component of the vector
-     * 
-     */
+
+
     public void printLength(int x, int y, int z) {
         System.out.println(Math.sqrt(x * x + y * y + z * z));
     }
-    public MathStuff(boolean sign, double mantissa, int exponent){
-        this.sign=sign;
-        this.mantissa=mantissa;
-        this.exponent=exponent;
-    }
-    public MathStuff(){
-        this.sign=true;
-        this.mantissa=0;
-        this.exponent=1;
-    }
-
     public void printSum(int x, int y, int z) {
         System.out.println(x + y + z);
     }
-
-    public void printMax(int x, int y, int z) {
-        System.out.println(Math.max(Math.max(x, y), z));
+    public static void main(String[] args) {
+        MathStuff mathStuff = new MathStuff();
+        mathStuff.printLength(1, 2, 3);
+        mathStuff.printSum(1, 2, 3);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private boolean sign;
+    private double mantissa;
+    private int exponent;
+
     public double calcValue(){
         return (sign ? 1 : -1) * mantissa * Math.pow(2, exponent);
     }
